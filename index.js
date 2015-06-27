6df23d9e487e43e611a1ca98e1178d199a7ceb49
LennyPenny <l.bernhardt@live.de>
2015-06-27 21:32:11 +0200

    fix code style

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ var Boss = require("./lib/boss.js");
 
 GLOBAL.boss = new Boss(process.env.TELEGRAM_BOT_TOKEN);
 
-let plugins = fs.readdirSync( "./plugins" );
+let plugins = fs.readdirSync("./plugins");
 
-plugins.forEach( function( plugin ) {
-	if ( path.extname( plugin ) != ".js" )
+plugins.forEach( function(plugin) {
+	if (path.extname( plugin ) != ".js")
 		return;
 	try {
-		require( "./plugins/" + plugin );
-		console.log( "Loaded " + plugin );
-	} catch ( e ) {
-		console.trace( e );
+		require("./plugins/" + plugin);
+		console.log("Loaded " + plugin);
+	} catch (e) {
+		console.trace(e);
 	}
 });
 
