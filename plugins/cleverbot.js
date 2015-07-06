5dc15e71106528e93b2106c87fe44eeeb3b970de
LennyPenny <l.bernhardt@live.de>
2015-07-06 21:52:54 +0200

    set up azure

---
+++ plugins/cleverbot.js
@@ -0,0 +1,15 @@
+var Cleverbot = require("cleverbot-node");
+var cleverbot = new Cleverbot;
+cleverbot.prepare();
+boss.on("message", (msg) => {
+	if (!msg.text.includes("boss") && !msg.text.includes("Boss")) {return;}
+	
+	msg.text = msg.text.replace(/\bBoss\b/ig, "Cleverbot");
+
+	cleverbot.write(msg.text, (res) => {
+		boss.sendMessage({
+			text: res.message.replace(/\bCleverbot\b/ig, "Boss"),
+			chat_id: msg.chat.id
+		})
+	})
+})
