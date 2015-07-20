707165eb777844d4d5de9064e94d8c994bb8bb91
LennyPenny <l.bernhardt@live.de>
2015-07-20 13:16:40 +0200

    added command check for cleverbot

--- plugins/cleverbot.js
+++ plugins/cleverbot.js
@@ -2,7 +2,7 @@ var Cleverbot = require("cleverbot-node");
 var cleverbot = new Cleverbot;
 cleverbot.prepare();
 boss.on("message", (msg) => {
-	if (!msg.text.includes("boss") && !msg.text.includes("Boss")) {return;}
+	if (msg.text.startsWith("/") || !msg.text.includes("boss") && !msg.text.includes("Boss")) {return;}
 	
 	msg.text = msg.text.replace(/\bBoss\b/ig, "Cleverbot");
 
